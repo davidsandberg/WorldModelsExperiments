3a6b05c8d7cff2a7d088c7f93df2d993511179df
David Sandberg <david.o.sandberg@gmail.com>
2018-12-03 20:42:02 +0100

    Input pipeline for series generation script

--- carracing/series.py
+++ carracing/series.py
@@ -5,47 +5,36 @@ all the dataset files into one dataset called series/series.npz
 
 import numpy as np
 import os
-import json
 import tensorflow as tf
-import random
+from tensorflow.python.data import Dataset
 from vae.vae import ConvVAE, reset_graph
+import time
 
 os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
-DATA_DIR = "record"
+DATA_DIR = "record/20181130-233040"
 SERIES_DIR = "series"
 model_path_name = "tf_vae"
 
 if not os.path.exists(SERIES_DIR):
     os.makedirs(SERIES_DIR)
+    
+def gen(filelist, path):
+  for fn in filelist:
+    o = np.load(os.path.join(path, fn))['obs']
+    a = np.load(os.path.join(path, fn))['action']
+    for i in range(o.shape[0]):
+      yield (o[i,:,:,:], a[i,:])
 
-def load_raw_data_list(filelist):
-  data_list = []
-  action_list = []
-  counter = 0
-  for i in range(len(filelist)):
-    filename = filelist[i]
-    raw_data = np.load(os.path.join(DATA_DIR, filename))
-    data_list.append(raw_data['obs'])
-    action_list.append(raw_data['action'])
-    if ((i+1) % 1000 == 0):
-      print("loading file", (i+1))
-  return data_list, action_list
-
-def encode_batch(batch_img):
-  simple_obs = np.copy(batch_img).astype(np.float)/255.0
-  simple_obs = simple_obs.reshape(batch_size, 64, 64, 3)
-  mu, logvar = vae.encode_mu_logvar(simple_obs)
-  z = (mu + np.exp(logvar/2.0) * np.random.randn(*logvar.shape))
-  return mu, logvar, z
-
-def decode_batch(batch_z):
-  # decode the latent vector
-  batch_img = vae.decode(z.reshape(batch_size, z_size)) * 255.
-  batch_img = np.round(batch_img).astype(np.uint8)
-  batch_img = batch_img.reshape(batch_size, 64, 64, 3)
-  return batch_img
-
+def create_dataset(filelist, path, nrof_epochs, shuffle_buffer_size, batch_size):
+  ds1 = Dataset.from_generator(lambda: gen(filelist, path), (tf.float32, tf.float32), (tf.TensorShape([64, 64, 3]), tf.TensorShape([3,])))
+  ds2 = Dataset.range(10000000)
+  ds = tf.data.Dataset.zip((ds1, ds2))
+  ds = ds.repeat(nrof_epochs)
+  ds = ds.prefetch(shuffle_buffer_size)
+  #ds = ds.shuffle(shuffle_buffer_size)
+  ds = ds.batch(batch_size)
+  return ds
 
 # Hyperparameters for ConvVAE
 z_size=32
@@ -57,11 +46,20 @@ filelist = os.listdir(DATA_DIR)
 filelist.sort()
 filelist = filelist[0:10000]
 
-dataset, action_dataset = load_raw_data_list(filelist)
+reset_graph()
+
+shuffle_buffer_size = 10000
 
 reset_graph()
 
-vae = ConvVAE(z_size=z_size,
+dataset = create_dataset(filelist, DATA_DIR, 1, shuffle_buffer_size, batch_size)
+
+# Create an iterator over the dataset
+iterator = dataset.make_one_shot_iterator()
+
+(obs, action), index = iterator.get_next()
+
+vae = ConvVAE(obs, z_size=z_size,
               batch_size=batch_size,
               learning_rate=learning_rate,
               kl_tolerance=kl_tolerance,
@@ -73,13 +71,27 @@ vae.load_json(os.path.join(model_path_name, 'vae.json'))
 
 mu_dataset = []
 logvar_dataset = []
-for i in range(len(dataset)):
-  data_batch = dataset[i]
-  mu, logvar, z = encode_batch(data_batch)
-  mu_dataset.append(mu.astype(np.float16))
-  logvar_dataset.append(logvar.astype(np.float16))
-  if ((i+1) % 100 == 0):
-    print(i+1)
+action_dataset = []
+try:
+    print('Started')
+    t = time.time()
+    # Keep running next_batch till the Dataset is exhausted
+    i = 0
+    t = time.time()
+    while True:
+        mu, logvar, action_, index_ = vae.sess.run([vae.mu, vae.logvar, action, index])
+        if not ((index_[1:] - index_[:-1])==1).all():
+          raise ValueError('Examples are out-of-order')
+        i += 1
+        mu_dataset.append(mu.astype(np.float16))
+        logvar_dataset.append(logvar.astype(np.float16))
+        action_dataset.append(action_)
+        print("step=%-8d time=%-8.4f batch_size=%-8d" % (i, (time.time()-t), mu.shape[0]))
+        t = time.time()
+        
+except tf.errors.OutOfRangeError:
+    print('Done!')
+
 
 action_dataset = np.array(action_dataset)
 mu_dataset = np.array(mu_dataset)
